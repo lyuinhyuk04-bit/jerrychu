860d1bab61593a752274703e86c1e87b255a37f0
Kain2 <lyuinhyuk04@gmail.com>
2026-06-07 13:55:29 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,98 @@
+const JERRY_DATA = {
+    "updated_at": "2026-06-07 13:49:35",
+    "is_live": false,
+    "notice_text": "제리가 옵니다\n\n\n \n말씀드렸던대로\n8일까지는 낮방을 챙겨보려고해요 ㅎ-ㅎ❤️\n \n9일부터는 챈나룽이 있어서\n저녁뱅일 것 같아용\n \n그 전까지는 \n낮방 야무지개 챙겨볼게여\n \n오늘은 고정시간인\n오후 12시에 오도록 하겠습니당\n \n와서 소통하다가\n카페나 시네티 볼 수 있움\n \n그럼 오늘도 쫗은하루 보내세요~~\n \n(아 그리고 33, 330개 챈나룽 방셀)\n많관부❤️",
+    "images": [],
+    "notices": [
+        {
+            "title": "공지어 낮방에",
+            "date": "2026-06-07 10:23:57",
+            "url": "https://www.sooplive.com/station/rariruro/post/197973687",
+            "content": "제리가 옵니다\n\n\n \n말씀드렸던대로\n8일까지는 낮방을 챙겨보려고해요 ㅎ-ㅎ❤️\n \n9일부터는 챈나룽이 있어서\n저녁뱅일 것 같아용\n \n그 전까지는 \n낮방 야무지개 챙겨볼게여\n \n오늘은 고정시간인\n오후 12시에 오도록 하겠습니당\n \n와서 소통하다가\n카페나 시네티 볼 수 있움\n \n그럼 오늘도 쫗은하루 보내세요~~\n \n(아 그리고 33, 330개 챈나룽 방셀)\n많관부❤️"
+        },
+        {
+            "title": "공지안녕!",
+            "date": "2026-06-05 17:34:59",
+            "url": "https://www.sooplive.com/station/rariruro/post/197801679",
+            "content": "일어나있는데욤\n머지 ㅠ.ㅠ 오늘 왜이렇게 \n눈이 안떠져\n \n밤새 마우스랑 키보드\n컴퓨터 알아본다고\n조금자서 그런가바여\n \n일단 오늘은 밥 먹구 \n넉넉하게 오후 7시쯤\n키도록 하겠습니당\n \n잠이 안깨서 졸려할까바\n깨고올게용\n \n오늘은 즉흥이 제리\n하는날 이따뵈오요"
+        },
+        {
+            "title": "공지횬님들...진짜 ㄴㅓ무 무서운데",
+            "date": "2026-06-04 20:20:07",
+            "url": "https://www.sooplive.com/station/rariruro/post/197719415",
+            "content": "제발 챈나룽 업좀..\n부탁드려도 될까요...\n오늘이 마지막이래요 ....\n나 계속 떨어지네 ...\n둘다해죠... 무파 크로쓰...❤\n\n\n \nhttps://www.sooplive.com/station/chaenna02/post/196058089#comment_noti113983845\n\n\n<숩니찡>\n \nhttps://www.sooplive.com/station/chaenna02/post/196058089#comment_noti113975877\n\n\n<제리츄>"
+        },
+        {
+            "title": "공지빠밤.",
+            "date": "2026-06-04 08:26:07",
+            "url": "https://www.sooplive.com/station/rariruro/post/197681167",
+            "content": "일찍 일어났습니당\n빠밤.\n \n낮에 키고시픈데 마리에요.\n근데 오늘 오후 4시부터\n버인마을이 있다고 하네용?\n \n문제는 아직 초대를 못 받았어..ㅎㅎ\n그으래서 오늘할 수 있을지는\n모르겠넴\n \n그래서 낮방하게되면\n백퍼 피곤할거라\n \n오늘은 오후 5시 30분쯤 오도록\n하겠습니당.\n \n사실 요즘 새벽소통이\n재밌어서 넘 일찍키면\n피곤할까바ㅎㅡㅎ.. ❤️\n\n\n \n조금 여유있게 킬게욤\n \n아 그리구\n오늘 맛보기 룰렛 이써요.\n많관부 ㅎㅡㅎ\n \n오늘도 긍정적인 마인드로\n즐겁고 활기찬 하루\n보내봅시당!!\n \n식사 잘 챙겨드시구\n오늘도 쫗은하루 ~~ ❤️"
+        },
+        {
+            "title": "공지방송키면",
+            "date": "2026-06-02 23:49:12",
+            "url": "https://www.sooplive.com/station/rariruro/post/197543871",
+            "content": "올래말래 올래말랭\n올래말램?\n \n버인 애들이 배그하고있긴한데\n하고싶긴한뎀 ㅎ-ㅎ...?"
+        },
+        {
+            "title": "공지헉ㅎㅡㅎ",
+            "date": "2026-06-01 12:11:56",
+            "url": "https://www.sooplive.com/station/rariruro/post/197388235",
+            "content": "조은 점심!\n \n저 어제 종겜 뭐 나오는지\n기다리다 아침 6시 30분까지\n안잠...\n \n그래서 지금 조금 피곤해ㅎㅡㅎ...\n하.지.만\n \n오늘 오후 2시에\n버인멤버 전원 모이기로해서\n \n저는 오후 1시에 와서 소통하고\n연습하겠습니다.\n \n근데 종겜항목에 크아가 나와써요.\nㅎㅡㅎ ❤️\n \n오늘 꼭 버인이 우승할 수 있도록\n파이팅 해볼게욤\n\n\n \n \n츄앙이들 6월도 함께 파이팅\n해보아요 5월 고생 많아써요 ❤️\n \n점심식사 챙겨드시구\n오늘도 쫗은하루"
+        }
+    ],
+    "schedule": [
+        {
+            "day": "월",
+            "date": "6/1",
+            "time": "오후 2:00 / 오후 1:00 방송",
+            "detail": "종겜"
+        },
+        {
+            "day": "화",
+            "date": "6/2",
+            "time": "방송 진행 (공지 확인)",
+            "detail": "배그"
+        },
+        {
+            "day": "수",
+            "date": "6/3",
+            "time": "공지 대기",
+            "detail": "소통 방송"
+        },
+        {
+            "day": "목",
+            "date": "6/4",
+            "time": "오후 4:00 / 오후 5:30 방송",
+            "detail": "소통 방송"
+        },
+        {
+            "day": "금",
+            "date": "6/5",
+            "time": "오후 7:00 방송",
+            "detail": "소통 방송"
+        },
+        {
+            "day": "토",
+            "date": "6/6",
+            "time": "공지 대기",
+            "detail": "소통 방송"
+        },
+        {
+            "day": "일",
+            "date": "6/7",
+            "time": "오후 12:00 방송",
+            "detail": "방셀"
+        }
+    ],
+    "fanarts": [
+        "https://stimg.sooplive.com/NORMAL_BBS/7/23077767/22646a1a66b0bc519.png",
+        "https://stimg.sooplive.com/NORMAL_BBS/7/23077767/94476a1a66ca75ead.png",
+        "https://stimg.sooplive.com/NORMAL_BBS/7/23077767/36936a19a35cbd8f6.png",
+        "https://stimg.sooplive.com/NORMAL_BBS/7/23077767/67126a196e22aa7d9.png",
+        "https://stimg.sooplive.com/NORMAL_BBS/7/23077767/96496a186bc3ef52c.png",
+        "https://stimg.sooplive.com/NORMAL_BBS/7/23077767/39776a183cbe15826.jpg",
+        "https://stimg.sooplive.com/NORMAL_BBS/7/23077767/88016a183cba49608.png",
+        "https://stimg.sooplive.com/NORMAL_BBS/7/23077767/14696a180b25eea38.png"
+    ]
+};
